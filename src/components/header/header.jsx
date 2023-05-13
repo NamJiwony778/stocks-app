b9e444137254135b59a8896c0c70f915aa93b308
Mariia <mariia@guarana.io>
2023-05-13 13:26:15 -0400

    header added to the app

---
+++ src/components/header/header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./header-styles.scss";
+
+const Header = () => {
+  return (
+    <nav className="header-container">
+      <h1>Stock Market Guru</h1>
+    </nav>
+  );
+};
+
+export default Header;
