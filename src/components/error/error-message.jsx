7494ea6bfa287b070a6ee5b8ea9baaa5061fa0c3
Mariia <brovarskamary@gmail.com>
2023-05-20 17:49:24 -0400

    log error added

--- src/components/error/error-message.jsx
+++ src/components/error/error-message.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
 const ErrorMessage = (props) => {
+  const { error } = props;
   return (
     <div className="loading-message-container">
       <span>Something wrong with API response</span>
+      {console.log("api error", error)}
     </div>
   );
 };
